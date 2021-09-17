c7d503dedaf6976a33d2fbc319cf081f360395ea
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2021-09-17 05:09:49 +0000

    add コメント

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '21e52830670a67e6d353b03f14db31fe6bf72b5a981ce0b03572d6280d1bcce0327f38f4f5aa01186e1e6f9cd9858545875dd5febc368c4f09647296cb5d8775'
