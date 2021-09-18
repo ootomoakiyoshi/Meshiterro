058dbe647ce72a043e3756f22575d0cc6b5d6672
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2021-09-18 10:29:45 +0000

    [Finish]4-5

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 end
